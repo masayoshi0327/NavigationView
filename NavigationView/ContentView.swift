448f531b7ab374b0c9ab3c16999a7e57c6b3134b
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-02-26 13:30:01 +0900

    コメント追加

--- NavigationView/ContentView.swift
+++ NavigationView/ContentView.swift
@@ -26,7 +26,7 @@ struct ContentView: View {
             //ここから
             NavigationView {
                 List(0..<4) { n in
-                    NavigationLink(regions[n], destination: Region(id: n, regionName: regions[n]))
+                    NavigationLink(regions[n], destination: Region(id: n, regionName: regions[n]))// 遷移のトリガーと遷移先のビューを指定
                 }
                 .navigationTitle("そらをとぶ")
             }
